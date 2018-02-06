aaa817e460dd35022987729af018c1b3df004ffe
Anwesh43 <anweshthecool0@gmail.com>
2018-02-07 02:24:29 +0530

    adding ButtonColorSwitchView in activity

--- app/src/main/java/ui/anwesome/com/kotlinbuttoncolorswitchview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinbuttoncolorswitchview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinbuttoncolorswitchview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.buttoncolorswitchview.ButtonColorSwitchView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view = ButtonColorSwitchView.create(this,arrayOf("hello","world","more","to","come","go"))
     }
 }
