185690cb629dbbe170d06e7352b54cba30b18fc4
Anwesh43 <anweshthecool0@gmail.com>
2018-02-07 02:14:28 +0530

    using Renderer in View

--- buttoncolorswitchview/src/main/java/ui/anwesome/com/buttoncolorswitchview/ButtonColorSwitchView.kt
+++ buttoncolorswitchview/src/main/java/ui/anwesome/com/buttoncolorswitchview/ButtonColorSwitchView.kt
@@ -19,13 +19,14 @@ fun getColorWithScale(target:Array<Int>,source:Array<Int>,scale:Float):Int {
 
 class ButtonColorSwitchView(ctx:Context,var texts: Array<String>):View(ctx) {
     val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = ButtonColorSwitchRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap(event.x,event.y)
             }
         }
         return true
