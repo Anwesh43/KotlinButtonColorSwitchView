eca43404b75d101403739d2cfd5b988309bed3a1
Anwesh43 <anweshthecool0@gmail.com>
2018-02-07 01:39:40 +0530

    adding buttoncolorswitch

--- buttoncolorswitchview/src/main/java/ui/anwesome/com/buttoncolorswitchview/ButtonColorSwitchView.kt
+++ buttoncolorswitchview/src/main/java/ui/anwesome/com/buttoncolorswitchview/ButtonColorSwitchView.kt
@@ -6,6 +6,15 @@ package ui.anwesome.com.buttoncolorswitchview
 import android.graphics.*
 import android.content.*
 import android.view.*
+
+
+fun getColorWithScale(target:Array<Int>,source:Array<Int>,scale:Float):Int {
+    val getColorPartAtIndex:(Int)->Int = {
+       source[it] + (scale*(target[it] - source[it])).toInt()
+    }
+    return Color.rgb(getColorPartAtIndex(0), getColorPartAtIndex(1), getColorPartAtIndex(2))
+}
+
 class ButtonColorSwitchView(ctx:Context):View(ctx) {
     val paint:Paint = Paint(Paint.ANTI_ALIAS_FLAG)
     override fun onDraw(canvas:Canvas) {
@@ -42,6 +51,33 @@ class ButtonColorSwitchView(ctx:Context):View(ctx) {
             if(animated) {
                 animated = false
             }
+        }
+    }
+    data class ButtonColorSwitch(var i:Int, var text:String, var x:Float, var y:Float, var w:Float, var h:Float) {
+        fun draw(canvas:Canvas,paint:Paint) {
+            var darkColorRGB:Array<Int> = arrayOf(38,50,56)
+            var whiteColorRGB:Array<Int> = arrayOf(255,255,255)
+            val blueColorRGB:Array<Int> = arrayOf(48,63,159)
+            val r = Math.max(w,h)/8
+            canvas.save()
+            canvas.translate(x,y)
+            paint.style = Paint.Style.FILL
+            paint.color = getColorWithScale(darkColorRGB,whiteColorRGB,0f)
+            canvas.drawRoundRect(RectF(-w/2,-h/2,w/2,h/2),r,r,paint)
+            paint.color = getColorWithScale(whiteColorRGB,darkColorRGB,0f)
+            paint.textSize = h/4
+            canvas.drawText(text,-paint.measureText(text)/2,h/8,paint)
+            paint.strokeWidth = Math.min(w,h)/30
+            paint.style = Paint.Style.STROKE
+            paint.color = getColorWithScale(blueColorRGB, darkColorRGB)
+            canvas.drawRoundRect(RectF(-w/2,-h/2,w/2,h/2),r,r,paint)
+            canvas.restore()
+        }
+        fun update(stopcb:(Float)->Unit) {
+
+        }
+        fun startUpdating(startcb:()->Unit) {
+
         }
     }
 }
