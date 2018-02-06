a7aac3dbd6f4564bc319a756610742373496f6dc
Anwesh43 <anweshthecool0@gmail.com>
2018-02-07 02:33:43 +0530

    adding ButtonSwitchListener in activity

--- app/src/main/java/ui/anwesome/com/kotlinbuttoncolorswitchview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinbuttoncolorswitchview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlinbuttoncolorswitchview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.buttoncolorswitchview.ButtonColorSwitchView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val view = ButtonColorSwitchView.create(this,arrayOf("hello","world","more","to","come","go"))
+        view.addButtonSwitchListener {
+            Toast.makeText(this,"$it selected",Toast.LENGTH_SHORT).show()
+        }
     }
 }
